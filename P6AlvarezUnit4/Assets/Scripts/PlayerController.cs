6a2de845a74049233d09bd42ec306c1705692665
741076-ship-it <741076@student.dusd.net>
2026-04-22 13:48:19 -0700

    end 4.1

---
+++ P6AlvarezUnit4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float speed = 5.0f;
+
+    private GameObject focalPoint;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
